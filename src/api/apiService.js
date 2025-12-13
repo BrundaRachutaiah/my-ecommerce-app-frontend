2b19e95b1005c62f2ae41bfc3ea853c1717bb6eb
Brunda D R <brundadr315@gmail.com>
2025-12-13 22:46:41 +0530

    removed the axios things

--- src/api/apiService.js
+++ src/api/apiService.js
@@ -1,14 +1,6 @@
 // src/api/apiService.js
 import axios from 'axios';
 
-axios.interceptors.request.use((config) => {
-  const token = localStorage.getItem('token');
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
-});
-
 const API_URL = import.meta.env.VITE_API_URL;
 
 // Get session ID from localStorage or create a new one
